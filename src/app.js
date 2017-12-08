d753fcbc2b563c58de4693a2007ea4425b1ea30f
Ilya Minyaev <ayli.veaynim@gmail.com>
2017-12-08 15:40:11 +0200

    Changes

--- src/app.js
+++ src/app.js
@@ -1,31 +1,44 @@
 require('cesium/Widgets/widgets.css');
 require('./css/styles.css');
 
+var axios = require('axios');
 var Cesium = require('cesium/Cesium');
 Cesium.BingMapsApi.defaultKey = "AiMlFcA_DUmJ-ShjLpX_mudqS6LFGw4r1rcCrYanKfSVTiOpmcFoNwEqKo04okVH";
 
 var viewer = new Cesium.Viewer('cesiumContainer');
-// cube http://localhost:8003/oulu/tilesets/sample
-// dragon http://localhost:8003/oulu/tilesets/dragon
-var tileset = viewer.scene.primitives.add(new Cesium.Cesium3DTileset({
-    url : 'http://localhost:8003/oulu/tilesets/sample',
-    maximumScreenSpaceError: 1000,
-    debugShowBoundingVolume: true,
-    debugShowGeometricError: true,
-    debugColorizeTiles: true,
-    debugShowRenderingStatistics: true,
-    debugShowUrl: true
-}));
+axios.get("http://localhost:8003/oulu/api/city/59fae4f73730003a7c10e6a3")
+    .then(function (response) {
+        var url = "http://localhost:8003" + response.data.url;
+        var tileset = viewer.scene.primitives.add(new Cesium.Cesium3DTileset({
+            url: url,
+            maximumScreenSpaceError: 1000,
+            debugShowBoundingVolume: true,
+            debugShowGeometricError: true,
+            debugColorizeTiles: true,
+            debugShowRenderingStatistics: true,
+            debugShowUrl: true,
+            skipLevelOfDetail: false
+        }));
 
-tileset.loadProgress.addEventListener(function(numberOfPendingRequests, numberOfTilesProcessing) {
-    if ((numberOfPendingRequests === 0) && (numberOfTilesProcessing === 0)) {
-        console.log('Stopped loading');
-        return;
-    }
-    console.log('Loading: requests: ' + numberOfPendingRequests + ', processing: ' + numberOfTilesProcessing);
-});
+        tileset.loadProgress.addEventListener(function (numberOfPendingRequests, numberOfTilesProcessing) {
+            if ((numberOfPendingRequests === 0) && (numberOfTilesProcessing === 0)) {
+                console.log('Loading completed.');
+                return;
+            }
+            console.log('Loading: requests: ' + numberOfPendingRequests + ', processing: ' + numberOfTilesProcessing);
+        });
 
-tileset.readyPromise.then(function(tileset) {
-    viewer.camera.viewBoundingSphere(tileset.boundingSphere, new Cesium.HeadingPitchRange(0, -0.5, 0));
-    viewer.camera.lookAtTransform(Cesium.Matrix4.IDENTITY);
-});
+        tileset.readyPromise.then(function (tileset) {
+            console.log(viewer.scene.primitives);
+            viewer.camera.viewBoundingSphere(tileset.boundingSphere, new Cesium.HeadingPitchRange(0, -0.5, 0));
+            viewer.camera.lookAtTransform(Cesium.Matrix4.IDENTITY);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+
+    })
+    .catch(function (error) {
+
+    });
+
